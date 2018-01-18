f3d86d28f166b8b3f0c236aa691c2ac427fb1663
laotou <1178661513@qq.com>
2018-01-18 11:28:20 +0800

    IDEA 与 github 连接测试

---
+++ src/main/java/com/springStudy/Demo.java
@@ -0,0 +1,17 @@
+package com.springStudy;
+
+public class Demo {
+   private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Demo(String name) {
+        this.name = name;
+    }
+}
